bf204364d075240eb1d5f86a18cde6e28b14aedb
Dell <Dell@DESKTOP-0T2HQTO>
2022-08-09 16:00:35 +0530

    ver2

--- DAY3Project/src/com/mybank/service/LoanAccountService.java
+++ DAY3Project/src/com/mybank/service/LoanAccountService.java
@@ -7,54 +7,17 @@ public class LoanAccountService {
 	private int time;
 	
 	
-	public int calculateInterest (int LoanId , int LoanAmount, int RateOfInterest, int Time ) {
-		loanId = LoanId;
-		loanAmount = LoanAmount;
-		rateOfInterest =RateOfInterest;
-		time = Time;
+	public int calculateInterest ( int Amount, int roi, int period ) {
+	
+		loanAmount = Amount;
+		rateOfInterest =roi;
+		time = period;
 		int SI =(loanAmount*rateOfInterest*time)/100;
 		return SI;
 		
 	}
 
 
-	public int getLoanId() {
-		return loanId;
-	}
-
-
-	public void setLoanId(int loanId) {
-		this.loanId = loanId;
-	}
-
-
-	public int getLoanAmount() {
-		return loanAmount;
-	}
-
-
-	public void setLoanAmount(int loanAmount) {
-		this.loanAmount = loanAmount;
-	}
-
-
-	public int getRateOfInterest() {
-		return rateOfInterest;
-	}
-
-
-	public void setRateOfInterest(int rateOfInterest) {
-		this.rateOfInterest = rateOfInterest;
-	}
-
-
-	public int getTime() {
-		return time;
-	}
-
-
-	public void setTime(int time) {
-		this.time = time;
-	}
+	
 
 }
--- DAY3Project/src/com/mybank/view/MainView.java
+++ DAY3Project/src/com/mybank/view/MainView.java
@@ -4,9 +4,9 @@ public class MainView {
 	
 	public static void main(String[] args)
 	{
-		LoanAccountService Interest1 = new LoanAccountService();
-		int Interest = Interest1.calculateInterest(126, 10000, 6, 4);
-		System.out.println("Interest for loan ID "+Interest1.getLoanId()+" is "+ Interest);
+		LoanAccountService interest1 = new LoanAccountService();
+		int Interest = interest1.calculateInterest( 10000, 6, 6);
+		System.out.println("Interest for   is "+ Interest);
 	}
 
 }
